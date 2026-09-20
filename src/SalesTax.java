0b083e7e60589e1cd71f03e8b26f6134e75caa05
meiersc <meiersc19@gmail.com>
2026-09-20 19:31:56 -0400

    Lab

---
+++ src/SalesTax.java
@@ -0,0 +1,8 @@
+public class SalesTax {
+    static void main(String[] args) {
+double ItemPrice= 15;
+final double TaxRate= 0.05;
+double TaxAmount= ItemPrice*TaxRate;
+                System.out.println("The tax price for this item is "+TaxAmount );
+    }
+}
